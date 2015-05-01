a0353ff5e01f79dc5803d60113417cc3a6e682a4
Adam Crownoble <adam.crownoble@biola.edu>
2015-05-01 15:30:16 -0700

    Check a local database for used NetIDs

--- lib/netid.rb
+++ lib/netid.rb
@@ -1,18 +1,10 @@
 class NetID
   class TrogdirAPIError < StandardError; end
 
-  attr_reader :netid
+  include Mongoid::Document
+  include Mongoid::Timestamps
 
-  def initialize(netid)
-    @netid = netid
-  end
+  field :netid, type: String
 
-  def available?
-    # TODO: check a local database instead of trogdir.
-    # This will prevent us from reusing NetIDs that have been renamed or deleted.
-    response = Trogdir::APIClient::People.new.by_id(id: netid, type: :netid).perform
-    raise TrogdirAPIError, response.parse['error'] if response.server_error?
-
-    response.status == 404
-  end
+  validates :netid, presence: true
 end
