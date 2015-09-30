22ab1f4e32336a00a5cd387c919af3192e7abb81
Michael Stephens <michael.stephens@biola.edu>
2015-09-30 11:21:32 -0700

    Upgrade sidekiq, use sidekiq-cron

--- Gemfile
+++ Gemfile
@@ -1,14 +1,13 @@
 source 'https://rubygems.org'
 
 gem 'activesupport'
-gem 'blazing'
 gem 'logging', '~> 1.8' # 2.0 is not compatible with blazing
 # beta1 fixes this issue https://github.com/railsconfig/rails_config/pull/86
 gem 'mongoid'
 gem 'rails_config', '~> 0.5.0.beta1'
 gem 'rake'
 gem 'sidekiq'
-gem 'sidetiq'
+gem 'sidekiq-cron'
 gem 'trogdir_api_client'
 
 group :development, :test do
--- lib/netid_syncinator.rb
+++ lib/netid_syncinator.rb
@@ -24,6 +24,11 @@ module NetIDSyncinator
       config.redis = { url: Settings.redis.url, namespace: 'netid-syncinator' }
     end
 
+    schedule_file = "config/schedule.yml"
+    if File.exists?(schedule_file)
+      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    end
+
     TrogdirAPIClient.configure do |config|
       config.scheme = Settings.trogdir.scheme
       config.host = Settings.trogdir.host
